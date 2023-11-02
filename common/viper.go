fed1de02fadffe80f9d81c26a81a7c700886c5af
gooopher <yu2nshuai@foxmail.com>
2023-11-02 18:27:43 +0800

    fix viper load

--- common/viper.go
+++ common/viper.go
@@ -4,37 +4,48 @@ import (
 	"infer-microservices/common/flags"
 	"infer-microservices/utils/logs"
 
+	"strings"
+
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
+
+var viperConfig *viper.Viper
 
 func init() {
 	flagFactory := flags.FlagFactory{}
 	flagViper := flagFactory.CreateFlagViper()
 
-	viper.SetConfigName(*flagViper.GetConfigName())
-	viper.SetConfigType(*flagViper.GetConfigType())
-	viper.AddConfigPath(*flagViper.GetConfigPath())
+	viperConfig = viper.New()
+	viperConfig.SetConfigName(*flagViper.GetConfigName())
+	viperConfig.SetConfigType(*flagViper.GetConfigType())
+	viperConfig.AddConfigPath(*flagViper.GetConfigPath())
 }
 
 func loadViperConfigFile() {
-	err := viper.ReadInConfig()
+	err := viperConfig.ReadInConfig()
 	if err != nil {
 		logs.Error(err)
 	}
-	userId := viper.GetString("useId")
-	itemId := viper.GetString("itemId")
+	userIdStr := viperConfig.GetString("userIdList") // "user1,user2,user3"
+	itemIdStr := viperConfig.GetString("itemIdList") //"item1,item2,item3"
 
-	bloomPush(GetUserBloomFilterInstance(), userId)
-	bloomPush(GetUserBloomFilterInstance(), itemId)
+	userIdList := strings.Split(userIdStr, ",")
+	itemIdList := strings.Split(itemIdStr, ",")
+
+	for _, userId := range userIdList {
+		bloomPush(GetUserBloomFilterInstance(), userId)
+	}
+
+	for _, itemId := range itemIdList {
+		bloomPush(GetUserBloomFilterInstance(), itemId)
+	}
 }
 
 func WatchBloomConfig() {
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
+	viperConfig.WatchConfig()
+	viperConfig.OnConfigChange(func(e fsnotify.Event) {
 		logs.Info("Config file changed:", e.Name) //the file only contains new users and new items in past 1 hour.
-
 		loadViperConfigFile()
-
 	})
 }
