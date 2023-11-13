c15475e1bcb28cadc024152b6ab48640f58a8fd7
gooopher <yu2nshuai@foxmail.com>
2023-11-13 17:09:41 +0800

    use reflect to parse unknown type recall_num

--- cores/service_config_loader/faiss_config_loader/faiss_config_loader.go
+++ cores/service_config_loader/faiss_config_loader/faiss_config_loader.go
@@ -3,6 +3,9 @@ package faiss_config_loader
 import (
 	faiss_index "infer-microservices/common/faiss_gogofaster"
 	"infer-microservices/utils"
+	"infer-microservices/utils/logs"
+	"reflect"
+	"strconv"
 )
 
 type FaissIndexConfig struct {
@@ -49,10 +52,33 @@ func (f *FaissIndexConfig) ConfigLoad(dataId string, indexConfStr string) error 
 		return err
 	}
 
+	//INFO:the recallNum param from http request,maybe int/ float /string。 user reflect to convert to int32.
+	recallNum := int32(100)
 	indexInfo := dataConf["indexInfo"].(map[string]interface{})
 	for indexName, tmpIndexConf := range indexInfo { //only 1 index
 		tmpIndexConfMap := tmpIndexConf.(map[string]interface{})
-		recallNum := int32(tmpIndexConfMap["recall_num"].(float64))
+
+		//recallNum := int32(tmpIndexConfMap["recall_num"].(float64))
+		recallNumType := reflect.TypeOf(tmpIndexConfMap["recallNum"])
+		recallNumTypeKind := recallNumType.Kind()
+		switch recallNumTypeKind {
+		case reflect.String:
+			recallNumStr, ok := tmpIndexConfMap["recallNum"].(string)
+			if ok {
+				recallNum64, err := strconv.ParseInt(recallNumStr, 10, 64)
+				if err != nil {
+					logs.Error(err)
+				} else {
+					recallNum = int32(recallNum64)
+				}
+			}
+		case reflect.Float32, reflect.Float64, reflect.Int16, reflect.Int, reflect.Int64, reflect.Int8,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			recallNum, _ = tmpIndexConfMap["recallNum"].(int32)
+		default:
+			logs.Info("unkown type, set recallnum to 100")
+		}
+
 		indexName_ := tmpIndexConfMap["index_name"].(string)
 		indexInfoStruct := &faiss_index.RecallRequest{
 			IndexName: indexName_,
