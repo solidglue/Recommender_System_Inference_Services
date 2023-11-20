91bfe6d790d1cd6f940c5109148f093e44edca13
gooopher <yu2nshuai@foxmail.com>
2023-11-20 17:46:30 +0800

    update

--- test/http_api_test.sh
+++ test/http_api_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl http://10.124.20.5:8650/infer -X POST -d 'data={"modelId":"deepfm-1|modelInfer","userId":"13438935173","itemIdList":["111","222","333","444"]}' 
+curl http://10.124.20.5:8650/infer -X POST -d 'data={"modelId":"deepfm-1|infer","userId":"112223333444555","itemIdList":["111","222","333","444"]}' 
 
