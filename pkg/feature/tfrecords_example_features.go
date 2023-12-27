89c27c23912ed30b2269942ef12e229e5676f514
gooopher <yu2nshuai@foxmail.com>
2023-12-27 17:31:25 +0800

    use kafka to create realtime feature

---
+++ pkg/feature/tfrecords_example_features.go
@@ -0,0 +1,12 @@
+package feature
+
+type SeqExampleBuff struct {
+	Key  *string
+	Buff *[]byte
+}
+
+type ExampleFeatures struct {
+	UserExampleFeatures        *SeqExampleBuff
+	UserContextExampleFeatures *SeqExampleBuff
+	ItemSeqExampleFeatures     *[]SeqExampleBuff
+}
